b604ca9a455e862b3ed1cc8b48dc3e2bef353903
Hasan Enes Güray <110453726+hasanenesguray@users.noreply.github.com>
2023-03-26 21:16:37 +0100

    Add files via upload

--- streamlit_mavi.py
+++ streamlit_mavi.py
@@ -6,7 +6,7 @@ import datetime
 # Sayfa Ayarları
 st.set_page_config(
     page_title="Sinemmetre",
-    page_icon="/Users/hasanenesguray/Desktop/Mavi/WhatsApp.jpeg",
+    page_icon="WhatsApp.jpeg",
     menu_items={
         "Get help": "mailto:hasanenesguray@gmail.com",
         "About": "For More Information\n" + "https://github.com/hasanenesguray/IDSA-DSBootcamp"
@@ -31,15 +31,15 @@ if st.sidebar.button("Submit"):
     
     if sevgi == 100:
         st.header("Bildiniz!")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220829-WA0005.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220726-WA0007.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220704-WA0010.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220704-WA0009.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220624-WA0006.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220624-WA0001.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220622-WA0013.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG-20220610-WA0000.jpg")
-        st.image("/Users/hasanenesguray/Desktop/Mavi/IMG_20220902_163044.jpg")
+        st.image("IMG-20220829-WA0005.jpg")
+        st.image("IMG-20220726-WA0007.jpg")
+        st.image("IMG-20220704-WA0010.jpg")
+        st.image("IMG-20220704-WA0009.jpg")
+        st.image("IMG-20220624-WA0006.jpg")
+        st.image("IMG-20220624-WA0001.jpg")
+        st.image("IMG-20220622-WA0013.jpg")
+        st.image("IMG-20220610-WA0000.jpg")
+        st.image("IMG_20220902_163044.jpg")
     else:
         st.header("Bilemediniz, lütfen tekrar deneyiniz!")
 
